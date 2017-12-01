70686e0aff609c0ebb883de6211c8bba7c9df014
Alejandro Roman <aromanarguello@gmail.com>
2017-11-30 20:53:29 -0500

    iteration 2 comments finished

--- starter-code/iteration2/iteration2/src/app/comments-component/comments-component.component.ts
+++ starter-code/iteration2/iteration2/src/app/comments-component/comments-component.component.ts
@@ -1,12 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-comments-component',
+  selector: 'comments',
   templateUrl: './comments-component.component.html',
   styleUrls: ['./comments-component.component.css']
 })
 export class CommentsComponentComponent implements OnInit {
 
+  commentTitle = "Comments"
+
+  myComments: any[] = [
+    {
+      comment: `Bootstrapping long tail seed round branding incubator
+                ramen supply chain beta return on investment success
+                deployment buzz gen-z startup.`,
+      authorName: "Alejandro"
+    },
+    {
+      comment: `Client churn rate android iteration user experience.
+                Direct mailing gamification research & development buzz
+                angel investor business-to-consumer channels scrum project
+                interaction design business plan lean startup pivot.`,
+      authorName: "Pedro"
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
--- starter-code/iteration2/iteration2/src/app/maincomponent/maincomponent.component.ts
+++ starter-code/iteration2/iteration2/src/app/maincomponent/maincomponent.component.ts
@@ -7,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MaincomponentComponent implements OnInit {
 
+
+      img       = "https://media.giphy.com/media/MEvbm06UZv32w/giphy.gif";
+
+      paragraph = `Series A financing ramen iteration twitter early adopters
+                   investor analytics growth hacking. Lean startup long tail technology.
+                   Analytics business-to-consumer twitter product management. Hypotheses
+                   paradigm shift first mover advantage lean startup pitch return on
+                   investment leverage focus monetization bandwidth long tail agile
+                   development responsive web design. Facebook agile development marketing
+                   incubator MVP.`
+
+      title     = "Learn to code with Ironhack 💩"
+
   constructor() { }
 
   ngOnInit() {
